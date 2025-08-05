e586994c066541053a46412afdc14659ca06364a
prakarsha5 <prakarsha.k@gmail.com>
2025-08-05 23:09:28 +0530

    Day4

---
+++ Day4.py
@@ -0,0 +1,52 @@
+class Solution(object):
+    def subarraySum(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
+        count = 0
+        sums = 0
+        d = dict()
+        d[0] = 1
+        
+        for i in range(len(nums)):
+            sums += nums[i]
+            count += d.get(sums-k,0)
+            d[sums] = d.get(sums,0) + 1
+        
+        return(count) 
+class Solution(object):
+    def longestPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        if len(s) <= 1:
+            return s
+        
+        Max_Len=1
+        Max_Str=s[0]
+        for i in range(len(s)-1):
+            for j in range(i+1,len(s)):
+                if j-i+1 > Max_Len and s[i:j+1] == s[i:j+1][::-1]:
+                    Max_Len = j-i+1
+                    Max_Str = s[i:j+1]
+
+        return Max_Str
+class Solution(object):
+    def isValid(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        mapping = {')': '(', '}': '{', ']': '['}
+        stack = []
+
+        for char in s:
+            if char in mapping.values():
+                stack.append(char)
+            elif char in mapping:
+                if not stack or mapping[char] != stack.pop():
+                    return False
+        return not stack        
